92bfa8d2b5c36fdc9b19fc0ada8b8da65156f114
David J. M. Karlsen <david@davidkarlsen.com>
2021-03-12 17:39:09 +0100

    add dockerfile

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export TAG=v2.1.1
-
-docker build -f Dockerfile.driver . -t quay.io/evryfs/cloud-provider-vsphere-csi-driver:${TAG}
-docker build -f Dockerfile.syncer . -t quay.io/evryfs/cloud-provider-vsphere-csi-syncer:${TAG}
-docker push quay.io/evryfs/cloud-provider-vsphere-csi-driver:${TAG}
-docker push quay.io/evryfs/cloud-provider-vsphere-csi-syncer:${TAG}
